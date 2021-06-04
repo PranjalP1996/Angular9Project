3dab6eacccd4499b4506e55b6a02837b71871fc4
PranjalP <pranjal2823godase@gmail.com>
2021-06-04 17:09:12 +0530

    inital stage is done

---
+++ Angular9Proj/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector:'app-property-card',
+  templateUrl
+  :'property-card.Component.html',
+  styleUrls:['property-card.component.css']
+})
+export class PropertyCardComponent{
+
+  Property: any={
+        "Id":1,
+        "Type":"House",
+        "Price":15000
+      }
+}
