0d485591274963ff2f10745132e0d94c5acc4183
PranjalP <pranjal2823godase@gmail.com>
2021-06-04 19:26:52 +0530

    create add list component

--- Angular9Proj/src/app/property/property-card/property-card.component.ts
+++ Angular9Proj/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 
 @Component({
@@ -8,10 +8,5 @@ import { Component } from "@angular/core";
   styleUrls:['property-card.component.css']
 })
 export class PropertyCardComponent{
-
-  Property: any={
-        "Id":1,
-        "Type":"House",
-        "Price":15000
-      }
+  @Input() property:any
 }
--- Angular9Proj/src/app/property/property-list/property-list.component.ts
+++ Angular9Proj/src/app/property/property-list/property-list.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+  Properties: Array<any>=[
+    {"Id":1,"Type":"House","Price":15000,"Name":"1BHK"},
+    {"Id":2,"Type":"House","Price":18000,"Name":"2BHK"},
+    {"Id":3,"Type":"House","Price":10000,"Name":"3BHK"}]
 
   constructor() { }
 
